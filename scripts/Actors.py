4bc9c829d280bdfa1449e7287770f925e395105c
NBaoHa <92132758+NBaoHa@users.noreply.github.com>
2022-11-11 18:57:00 -0500

    Add files via upload

---
+++ scripts/Actors.py
@@ -0,0 +1,14 @@
+
+
+
+class Actors:
+    def __init__(self, type):
+        '''
+        Hospital (boundary polygon) --> centered to mean location pixel
+        
+        '''
+        pass
+
+
+    def get_centroid(self):
+        pass
