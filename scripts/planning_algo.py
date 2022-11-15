0d66d6541fd4d603a03109657ed077aaac20cfa9
NBaoHa <b9ha@uwaterloo.ca>
2022-11-14 23:25:38 -0500

    new_dev

--- scripts/planning_algo.py
+++ scripts/planning_algo.py
@@ -1,15 +1,14 @@
-import gdal as gd
-import ogr
-import osr
+
 import cv2
 import numpy as np
 import pandas as pd
 import random
 import matplotlib.pyplot as plt
+import math 
 
 
 
-def draw_route(c1, c2, map_img): # return lst of pixel of optimal route  ### does not mutate map_img
+def draw_route(c1, c2, map_img, scale_factor=10): # return lst of pixel of optimal route  ### does not mutate map_img
     '''
     c1/c2 : (x, y)
     map_img = [[pix, pix]]
@@ -18,15 +17,119 @@ def draw_route(c1, c2, map_img): # return lst of pixel of optimal route  ### doe
     img = map_img.copy()
     current = c1
     end = c2
-    route = [c1]
-    while not covered(img):
-        current = c1
-        while current != end:
-            pass
+    cost = {}
+    opened = {}
+    closed = {}
+    
+    
+    while current != end:
+        #print(current)
+        img[current[1]][current[0]] = 0
+        if closed == {}:
+            closed[current] = 0
+        candidates = add_cand(current, img)
 
-def covered(img):
-    for line in img:
-        for pix in line:
-            if pix != 999:
-                return False
-    return True
+
+        if end in candidates:
+            current = end
+            closed[current] = img[end[1]][end[0]]
+            img[current[1]][current[0]]=0
+            break
+
+        even_compare = {}
+        for cand in candidates:
+            if cand not in closed.keys():
+                x, y = cand
+                to_target_cost = dst_cords((x, y),end) * 10
+                from_current_cost = dst_cords(c1,(x,y)) * 10 #change c1 to current if dont want full coverage but c1 is the correct one
+                criteria_weight = img[y][x] * (scale_factor)
+                opened[cand]= [to_target_cost+from_current_cost+criteria_weight,to_target_cost]
+                
+
+        
+        even_cands={}
+        min_choice = min(opened.items(), key=lambda x: x[1][0]) 
+        
+        for cand in opened:
+            if opened[cand][0] == min_choice[1][0]:
+                even_cands[cand] = opened[cand][1] 
+        
+        if len(even_cands) > 1:
+            min_choice = min(even_cands, key=even_cands.get)
+            print(min_choice)
+            current = min_choice
+        else:
+            current=min_choice[0]
+
+        delete_node =opened.pop(current)
+        closed[current]= delete_node
+        
+    
+    if current == end:
+        closed[current] = 0
+        img[current[1]][current[0]]=0
+
+    plt.imshow(img)
+    plt.scatter([c1[0], c2[0]], [c1[1],c2[1]],c='r', marker=">")
+    plt.show()
+    return closed.keys()
+
+    # go one node and access the next by comparing weights (refurbish path plan)
+    
+    
+
+
+def add_cand(c1, img):
+    width = len(img[0])
+    height = len(img)
+ 
+    candidates = []
+    x,y = c1[0], c1[1]
+    if x > 0:
+        if img[y][x-1] !=0:
+            candidates.append((x-1, y))
+        if y > 0 and img[y-1][x-1] != 0:
+            candidates.append((x-1, y-1)) # top left pixel
+        if y < height -1 and img[y+1][x-1]:
+            candidates.append((x-1,y+1)) # bottom left pixel
+
+    if x < width-1:
+        if img[y][x+1] !=0:
+            candidates.append((x+1, y))
+        if y > 0 and img[y-1][x+1] != 0:
+            candidates.append((x+1, y-1)) # top right pixel
+        if y < height-1 and img[y+1][x+1] !=0:
+            candidates.append((x+1, y+1))
+    if y > 0:
+        if img[y-1][x] !=0:
+            candidates.append((x, y-1))
+    if y < height-1:
+        if img[y+1][x] !=0:
+            candidates.append((x, y+1))
+    return candidates
+
+def dst_cords(c1, c2):
+    x_dist = abs(c2[0] - c1[0])
+    y_dist = abs(c2[1]- c2[1])
+    dist = math.sqrt(x_dist**2 + y_dist**2)
+    return dist
+
+if __name__ == "__main__":
+    # img = [list(np.random.randint(low = 1,high=5,size=50))]
+    # while len(img) < 51:
+    #     img.append(list(np.random.randint(low = 1,high=5,size=50)))
+    # print(img)
+    img = [[4, 1, 3, 4, 2, 3, 4, 0, 0, 0],
+           [3, 1, 3, 5, 7, 9, 1, 3, 2, 3], 
+           [1, 3, 2, 1, 4, 2, 2, 2, 1, 3], 
+           [2, 1, 1, 4, 3, 4, 4, 8, 9, 4], 
+           [10, 1, 3, 2, 2, 2, 1, 1, 1, 2], 
+           [5, 10, 10, 9, 1, 1, 1, 1, 2, 2], 
+           [0, 0, 2, 11, 8, 1, 1, 1, 1, 4], 
+           [0, 0, 0, 3, 7, 1, 4, 3, 3, 4]]
+
+    image = cv2.imread('/home/bao/Map_unit/Map_unit/data/Result_700.png', 0)
+    print(image[:100])
+    plt.imshow(image)#[:100])
+    plt.show()
+    draw_route((150, 300), (450,450), image)#[:100])
--- scripts/process_map.py
+++ scripts/process_map.py
@@ -1,6 +1,4 @@
-import gdal as gd
-import ogr
-import osr
+
 import cv2
 import numpy as np
 import pandas as pd
@@ -14,7 +12,8 @@ class Pathplanning:
         # declare parameters
         self.mapdata = []  # initiate not numpy array yet
         self.img = cv2.imread(map_png, 0) # 0 means view as grayscale
-        #self.facil_mask = cv2.imread(facilities_png, 0)
+        self.img = self.img
+        self.facil_mask = cv2.imread(facilities_png, 0)
         self.weights = weights   # corresponding pixel val to weights
         self.ascending_gradient = ascending_gradient # signify that darker colors have higher weights 
         self.algo = algorithm
@@ -25,6 +24,9 @@ class Pathplanning:
         self.facility_nodes = {} # store nodes as Hashmap for faster computation
         self.facility_coords = [] # for easier acess in gen map   /// (x, y)
         self.weights_legend = {} # stores cost
+
+        #output variables
+        self.routes = {}
         
     def run(self):
         # create new map to store routes
@@ -34,8 +36,10 @@ class Pathplanning:
         
         
         self.gen_graph()   ## do all possible connections between all the reserves (can filter out later with Network Analysis)
-
+        print(self.routes.keys())
         # show result
+
+        self.gen_new_img()
         self.show_res()
 
 
@@ -43,6 +47,8 @@ class Pathplanning:
         # mark boundaries
         self.width = len(self.img[0])
         self.height = len(self.img)
+        # print(len(self.facil_mask[0]))  ## make sure two png are the same dim
+        # print(len(self.facil_mask))
         print('dimensions: ',self.width, self.height)
         self.new_canvas = [[0]*self.width]*self.height
 
@@ -78,45 +84,87 @@ class Pathplanning:
         place them into new_canvas
         '''
         ### HERE is to calculate centroid coordinates
+        curx,cury = 0,0
+        temp_coord = []
+        x_arr = []
+        y_arr = []
+        while cury < self.height:
+            while curx < self.width:
+                if self.facil_mask[cury][curx] == 255:
+                    self.facil_mask[cury][curx] = 100
+                    x_arr.append(curx)
+                    y_arr.append(cury)
+                    temp_coord.append((curx, cury))
+                
+                curx +=1
+            curx =0
+            cury+=1
 
+            ## get centroid
+
+        
+        
+        # xlang,ylang = x_arr,y_arr
+
+        # print(temp_coord)
+        # plt.imshow(self.img)
+        # plt.imshow(self.facil_mask)
+        # plt.scatter(xlang, ylang, c='r', marker=">")
+        # plt.show()
+        
+        #print(self.facil_mask[300:400][0])
         ## Draft delete below if the facilities png is available------  !!! can sychronously place facility centroids in blank canvas
-        num_facil = 10
-        while num_facil > 0:
-            smpx, smpy = int(random.uniform(0, self.width)), int(random.uniform(0, self.height))
-            while smpx in self.facility_nodes.keys() and smpy in self.facility_nodes[smpx]:
-                smpx, smpy = int(random.uniform(0, self.width)), int(random.uniform(0, self.height))
+        # num_facil = 2
+        # while num_facil > 0:
+        #     smpx, smpy = int(random.uniform(0, self.width)), int(random.uniform(0, self.height))
+        #     while smpx in self.facility_nodes.keys() and smpy in self.facility_nodes[smpx]:
+        #         smpx, smpy = int(random.uniform(0, self.width)), int(random.uniform(0, self.height))
             
-            if smpx in self.facility_nodes.keys():
-                self.facility_nodes[smpx].append(smpy)
-                self.img[smpy][smpx] = 0
-                self.facility_coords.append((smpx, smpy))
-            else:
-                self.facility_nodes[smpx] = [smpy]
-                self.facility_coords.append((smpx, smpy))
-                self.img[smpy][smpx] = 0
+        #     if smpx in self.facility_nodes.keys():
+        #         self.facility_nodes[smpx].append(smpy)
+        #         self.img[smpy][smpx] = 0
+        #         self.facility_coords.append((smpx, smpy))
+        #     else:
+        #         self.facility_nodes[smpx] = [smpy]
+        #         self.facility_coords.append((smpx, smpy))
+        #         self.img[smpy][smpx] = 0
 
-            num_facil -= 1
+        #     num_facil -= 1
         ##-----------------------------------------------------------
+        self.facility_coords=[(400, 300), (600, 200), (350, 400)]
         print('facilities (x, y): ', self.facility_nodes)
         print('facility coords: ', self.facility_coords)
+        
 
     def gen_graph(self):
         for c1 in self.facility_coords: # O(n^2)
             for c2 in self.facility_coords:
-                if c1 != c2:
-                    planning_algo.draw_route(c1, c2, self.img) # self.img is [[pix pix]]
-                    break
+                if c1 != c2 and ((c2, c1) not in self.routes.keys()):
+                    #print(self.img[c1[1]][c1[0]])
+                    # if self.img[c1[1]][c1[0]] != 0 and self.img[c2[1]][c2[0]] != 0:
+                    route = planning_algo.draw_route(c1, c2, self.img) # self.img is [[pix pix]]
+                    self.routes[(c1,c2)] = route
+
+    def gen_new_img(self):
+        for route in self.routes:
+            for coord in self.routes[route]:
+                x,y = coord[0], coord[1]
+                self.img[y][x] = 0
+
+
+                
 
 
     def show_res(self):
         cv2.imshow('image', self.img)
+        cv2.imshow("mask",self.facil_mask)
         cv2.waitKey(0)
 
 
 
 
 if __name__ == '__main__':
-    g = Pathplanning('/home/bao_wy/map_processing_unit/data/Result_700.png',
-    'this file contains raster that highlights reserves and hospitals in two different colors but need to be the same dim as the map_png', [1, 2, 3, 4], ascending_gradient=False)
+    g = Pathplanning('/home/bao/Map_unit/Map_unit/data/Sample_Res_700.png',
+    '/home/bao/Map_unit/Map_unit/data/reserves_700.png', [1, 2, 3, 4], ascending_gradient=False)
     g.run()
     
